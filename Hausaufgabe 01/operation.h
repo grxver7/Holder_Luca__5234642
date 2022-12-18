073faf59f4becc00ad5115c837cb468e687cbf0f
grxver7 <79474232+grxver7@users.noreply.github.com>
2022-12-18 19:27:12 +0100

    Delete Hausaufgabe 01 directory

--- Hausaufgabe 01/operation.h
+++
@@ -1,8 +0,0 @@
-#ifndef HAUSAUFGABE_01_OPERATION_H
-#define HAUSAUFGABE_01_OPERATION_H
-
-#include "complexNumberStructure.h"
-
-double operation (complexNumber first_Number, complexNumber second_Number, char selectedMenu);
-
-#endif //HAUSAUFGABE_01_OPERATION_H
--- Hausaufgabe 01/stringCutter.h
+++
@@ -1,6 +0,0 @@
-#ifndef HAUSAUFGABE_01_STRINGCUTTER_H
-#define HAUSAUFGABE_01_STRINGCUTTER_H
-
-double * stringCutter (char number[]);
-
-#endif //HAUSAUFGABE_01_STRINGCUTTER_H

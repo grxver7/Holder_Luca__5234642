0d294fc4afbd0e9fac83178aeae650c13bfa286b
grxver7 <79474232+grxver7@users.noreply.github.com>
2022-12-18 19:33:01 +0100

    Add files via upload

---
+++ Hausaufgabe 01/stringCutter.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+double *  stringCutter (char number[]){
+    double imaginaryNumber;
+    double realNumber;
+    char *token;
+    char **ptr;
+    double *numberReturn =malloc(sizeof (double) *2);
+
+    if ((strchr(number, '+') == NULL)) {
+        printf("Error, invalid input!");
+        return NULL;
+    }
+
+    token = strtok(number, "+"); //first part before '+'
+
+    if (strchr(token, 'i') != NULL) { //imaginary number is first number
+        imaginaryNumber = strtod(token, &ptr); //cast to double
+        numberReturn[1] = imaginaryNumber; //storage [1] = imaginary
+
+        token = strtok(NULL, "+"); //part after '+'
+        token = strtok(token, "i"); //cut i
+        realNumber = strtod(token, &ptr);
+        numberReturn[0] = realNumber; //storage [0] = real
+    }
+    else {
+        token=token;
+        realNumber = strtod(token, &ptr);
+        numberReturn[0] = realNumber;
+
+        token = strtok(NULL, "+");
+        token = strtok(token, "i");
+        imaginaryNumber= strtod(token, &ptr);
+        numberReturn[1] = imaginaryNumber;
+    }
+
+    return numberReturn;
+}

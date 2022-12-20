6f5095487cb0b0bdaa3b051659a75b04059c1cfc
grxver7 <79474232+grxver7@users.noreply.github.com>
2022-12-20 21:13:28 +0100

    Delete Hausaufgabe 01 directory

--- Hausaufgabe 01/complexNumberStructure.h
+++
@@ -1,9 +0,0 @@
-#ifndef HAUSAUFGABE_01_COMPLEXNUMBERSTRUCTURE_H
-#define HAUSAUFGABE_01_COMPLEXNUMBERSTRUCTURE_H
-
-typedef struct ComplexNumber {
-        double real;
-        double imaginary;
-} complexNumber;
-
-#endif //HAUSAUFGABE_01_COMPLEXNUMBERSTRUCTURE_H
--- Hausaufgabe 01/displayMenu.h
+++
@@ -1,4 +0,0 @@
-#ifndef HAUSAUFGABE_01_DISPLAYMENU_H
-#define HAUSAUFGABE_01_DISPLAYMENU_H
-void displayMenu(complexNumber first_Number, complexNumber second_Number, complexNumber result, char selectedMenu, char operation);
-#endif //HAUSAUFGABE_01_DISPLAYMENU_H

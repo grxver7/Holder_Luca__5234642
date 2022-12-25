6550622dc061b7ef1e4cbc7088ce59a5e2600f13
Luca Holder <79474232+grxver7@users.noreply.github.com>
2022-12-25 22:53:24 +0100

    Delete Hausaufgabe 02/Hausaufgabe 02 directory

--- Hausaufgabe 02/Hausaufgabe 02/functionF.h
+++
@@ -1,6 +0,0 @@
-#ifndef HAUSAUFGABE_02_FUNCTIONF_H
-#define HAUSAUFGABE_02_FUNCTIONF_H
-double functionF (double x);
-double derivationFunctionF (double x);
-double derivationFunctionNumeric(double x);
-#endif //HAUSAUFGABE_02_FUNCTIONF_H

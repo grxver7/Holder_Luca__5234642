1afb8f1c99cb7dc345b10f06d6f064dd0f0b1a24
Luca Holder <79474232+grxver7@users.noreply.github.com>
2022-12-25 22:54:42 +0100

    Add files via upload

---
+++ Hausaufgabe 02/functionF.h
@@ -0,0 +1,6 @@
+#ifndef HAUSAUFGABE_02_FUNCTIONF_H
+#define HAUSAUFGABE_02_FUNCTIONF_H
+double functionF (double x);
+double derivationFunctionF (double x);
+double derivationFunctionNumeric(double x);
+#endif //HAUSAUFGABE_02_FUNCTIONF_H

99cafa2a4cb10b017d682d0e07d14e7168f9be28
Luca Holder <79474232+grxver7@users.noreply.github.com>
2022-12-25 22:54:27 +0100

    Delete Hausaufgabe 02 directory

--- Hausaufgabe 02/resultStruct.h
+++
@@ -1,8 +0,0 @@
-#ifndef HAUSAUFGABE_02_RESULTSTRUCT_H
-#define HAUSAUFGABE_02_RESULTSTRUCT_H
-typedef struct Result {
-    int iterations;
-    double zeroOfFunction;
-
-}result;
-#endif //HAUSAUFGABE_02_RESULTSTRUCT_H
